61ac42a5f2e53ace12555d84a9c4198cca54be7a
Jonah's MacbookAir <puckybreg@gmail.com>
2020-02-04 20:22:00 -0500

    update config

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -1,22 +1,22 @@
-  import firebase from 'firebase/app';
-  import 'firebase/firestore';
-  import 'firebase/auth';
-  
-  
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBTnPj07Yv5h76HjFp6_sYJ90ESP9smxAY",
-    authDomain: "court-scraper.firebaseapp.com",
-    databaseURL: "https://court-scraper.firebaseio.com",
-    projectId: "court-scraper",
-    storageBucket: "court-scraper.appspot.com",
-    messagingSenderId: "240598763698",
-    appId: "1:240598763698:web:6fc0e676602d0215bba1ab"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCKn9udAB_jTlHqkBW7W-wXjXWkBVmaAQs",
+  authDomain: "courtscraper.firebaseapp.com",
+  databaseURL: "https://courtscraper.firebaseio.com",
+  projectId: "courtscraper",
+  storageBucket: "courtscraper.appspot.com",
+  messagingSenderId: "349391854426",
+  appId: "1:349391854426:web:724223d6327d03b3837163"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 //   firebase.analytics();
-  // firebase.firestore().settings({});
+// firebase.firestore().settings({});
 
 
-  export default firebase;
+export default firebase;
